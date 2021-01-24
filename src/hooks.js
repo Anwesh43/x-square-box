f51e30596dd9b2922b15cba71f63582e383681d0
Anwesh43 <anweshthecool0@gmail.com>
2021-01-24 19:16:29 +0530

    adding useStyle hook

--- src/hooks.js
+++ src/hooks.js
@@ -39,3 +39,53 @@ export const useDimension = () => {
         h
     }
 }
+
+export const useStyle = (w, h, scale) => {
+    const size = Math.min(w, h) / 10
+    const position = 'absolute'
+    const left = `${w / 2}px`
+    const top = `${h / 2}px`
+    const color = `indigo`
+    
+    const sf = Math.sin(Math.PI / 180 * scale)
+    return {
+        parentStyle() {
+            return {
+                left, 
+                top,
+                position 
+            }
+        },
+        boxStyle() {
+            const left = `${-size / 2}px`
+            const top = `${-size / 2}px`
+            const border = `2px solid ${color}`
+            const width = `${size}px`
+            const height = `${size}px`
+            return {
+                position, 
+                width, 
+                height, 
+                top,
+                left,
+                border 
+            }
+        },
+        lineStyle(i) {
+            const left = `${-size / 2}px`
+            const top = `0px`
+            const background = color 
+            const width = `${size}px`
+            const height = `${size / 15}px`
+            const WebkitTransform = `rotate(${45 * (1 - 2 * i) * sf}deg)`
+            return {
+                left,
+                width,
+                top, 
+                height, 
+                background, 
+                WebkitTransform
+            }
+        }
+    }
+}
